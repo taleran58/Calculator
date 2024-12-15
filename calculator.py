c31b2ac241011d6eb3cd1a45ddac5b06b53f5559
taleran58@gmail.com <taleran58@gmail.com>
2024-12-15 15:31:46 +0200

    add power function

--- calculator.py
+++ calculator.py
@@ -14,16 +14,21 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
+# This function powers two numbers
+def divide(x, y):
+    return x ** y
+
 
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+print("5.Power")
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice(1/2/3/4/5): ")
 
     # check if choice is one of the four options
     if choice in ('1', '2', '3', '4'):
@@ -45,6 +50,9 @@ while True:
 
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
+        
+        elif choice == '5':
+            print(num1, "**", num2, "=", power(num1, num2))
         
         # check if user wants another calculation
         # break the while loop if answer is no
